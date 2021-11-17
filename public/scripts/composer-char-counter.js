815d7aed302fc5b644844d68e4ad63527f47bc33
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-17 03:07:24 +0000

    add tweets and fix nav bar at top

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,21 @@
-$(document).ready(function() {
- 
+$(document).ready(function () {
+
   const $textArea = $('.new-tweet');
   const $textInput = $('#tweet-text');
-  
-  $textArea.on('input', function() {
+
+  $textArea.on('input', function () {
     const inputLength = $textInput.val().length;
     const charsLeft = 140 - inputLength;
-    const counter= $(this).find(".counter");
-    counter.val(charsLeft);
-    if(counter.val() < 0){
-      counter.addClass("negative-val").removeClass("counter");
-    }
+    const counter = $(this).find(".counter");
+    counter.text(charsLeft);
     
-    console.log(charsLeft);
-    console.log(counter.html())
-
-   
+    if (charsLeft < 0) {
+      counter.addClass("negative-val");
+    }
+    else {
+      counter.removeClass("negative-val")
+    }
   })
- 
-
 
 });
 
-// $(this).parentsUntil(".new-tweet").find(".counter").html()
