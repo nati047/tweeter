0a62b91b54f1baeb0e56d69bb64c27a1bf282327
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-17 19:52:36 +0000

    add tweets dynamicly using js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,50 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
+
+  const createTweetElement = (tweetObj) => {
+    const $tweet = $(`<article class="tweets-container"></article>`);
+    const $header = $(`<header class="tweet-header"></header>`);
+    const $nameAndAvater = $(`<div class="name">
+      <img src=${tweetObj["user"].avatars} width="50" height="50">&nbsp;${tweetObj["user"].name}
+      </div>
+      <h4 class="user-name">${tweetObj["user"].handle}</h4>`);
+
+    const $paragraph = $(`<p calss="a-tweet" >${tweetObj["content"].text}</p>`)
+    const $footer = $(`<footer class="foot-text">
+    <div class="icons">
+      <i class="fa fa-flag" aria-hidden="true"></i>
+      <i class="fa fa-retweet" aria-hidden="true"></i>
+      <i class="fa fa-heart" aria-hidden="true"></i>
+      </div>
+    </footer>`);
+    const $footTimeStamp = $(`<div class="time-stamp">${tweetObj["created_at"]}</div>`)
+
+    $tweet.append($header);
+    $tweet.append($paragraph);
+    $tweet.append($footer);
+    $header.append($nameAndAvater);
+    $footer.prepend($footTimeStamp);
+
+    return $tweet;
+  };
+
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants!"
+    },
+    "created_at": 1461116232227
+  }
+
+  const $tweets = createTweetElement(tweetData);
+  $(".new-tweet").append($tweets);
+});
+
+
 
