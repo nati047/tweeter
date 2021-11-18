a96028e420ef93809ff80f7b356413dc0f9d8955
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-18 00:18:46 +0000

    fetch tweets using ajax get request and render them on the main page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,30 +33,7 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  
 
 const renderTweets = function(tweetsArr) {
   for( let tweet of tweetsArr) {
@@ -64,9 +41,9 @@ const renderTweets = function(tweetsArr) {
     $(".new-tweet").append($tweetArticle);
   }
 };
-renderTweets(data);
 
-// const $form = $(".submission-form")
+
+
 const $form = $("#submit-form");
 $form.on("submit", (event) =>{
   event.preventDefault();
@@ -86,6 +63,14 @@ $form.on("submit", (event) =>{
     console.log(err); 
   });
 });
+
+const loadTweets = function() {
+  $.ajax("/tweets", {
+    method: "GET"
+  }).then(function(data) {
+    renderTweets(data);
+  })
+}();
 
 
 });
